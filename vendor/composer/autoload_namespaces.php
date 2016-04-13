80e5e6137d3c7319c39f9858a936c3825d7c995b
Gaston Severina <gaston.e.severina@intel.com>
2016-04-13 17:52:03 -0300

    adding igorw for managing configurations

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -8,4 +8,5 @@ $baseDir = dirname($vendorDir);
 return array(
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
     'Pimple' => array($vendorDir . '/pimple/pimple/lib'),
+    'Igorw\\Silex' => array($vendorDir . '/igorw/config-service-provider/src'),
 );
--- web/index.php
+++ web/index.php
@@ -5,5 +5,11 @@ use Silex\Application;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Application();
-echo "HOLA";
-
+$env = getenv('APP_ENV') ?: 'prod';
+#echo $env;
+$app->register(
+    new Igorw\Silex\ConfigServiceProvider(
+        __DIR__ . "/../config/$env.json"
+    )
+);
+#print_r($app);
