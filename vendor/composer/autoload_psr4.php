ece7b0c345b91235197ff825083dfced6df22398
Gaston Severina <gaston.e.severina@intel.com>
2016-04-13 18:17:07 -0300

    adding monolog

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -13,4 +13,5 @@ return array(
     'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
     'Symfony\\Component\\Debug\\' => array($vendorDir . '/symfony/debug'),
     'Silex\\' => array($vendorDir . '/silex/silex/src/Silex'),
+    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
 );
--- web/index.php
+++ web/index.php
@@ -7,14 +7,21 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Application();
 $env = getenv('APP_ENV') ?: 'prod';
 #echo $env;
+
 $app->register(
     new Igorw\Silex\ConfigServiceProvider(
         __DIR__ . "/../config/$env.json"
     )
 );
+
 $app->register(
     new Silex\Provider\TwigServiceProvider(),
     ['twig.path' => __DIR__ . '/../views']
 );
-print_r($app);
 
+$app->register(
+    new Silex\Provider\MonologServiceProvider(),
+    ['monolog.logfile' => __DIR__ . '/../app.log']
+);
+
+print_r($app);
